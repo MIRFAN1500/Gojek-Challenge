ceceedb401da272f637712f72c4cb3ac79a9bb36
M Irfan <irfanaris771@gmail.com>
2022-03-10 10:48:22 +0800

    Badge Menu

--- app/src/main/java/com/binar/gojekchallenge/MainActivity.kt
+++ app/src/main/java/com/binar/gojekchallenge/MainActivity.kt
@@ -2,10 +2,18 @@ package com.binar.gojekchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.binar.gojekchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val menuItemIds = listOf(R.id.navigation_promo, R.id.navigation_chat)
+        menuItemIds.forEach { menuItemId ->
+            val badge = binding.navView.getOrCreateBadge(menuItemId)
+            badge.isVisible = true
+        }
     }
 }
